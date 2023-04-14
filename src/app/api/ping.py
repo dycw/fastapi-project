8342f54da3c6053e5a2848603e9d54ea3e516cbf
Derek Wan <d.wan@icloud.com>
2023-04-14 13:34:43 +0900

    Move Heroku push to master only, add `version` endpoint (#8)

--- src/app/api/ping.py
+++ src/app/api/ping.py
@@ -1,6 +1,7 @@
 from beartype import beartype
 from fastapi import APIRouter, Depends
 
+from app import __version__
 from app.config import Settings, get_settings
 
 router = APIRouter()
@@ -10,7 +11,13 @@ router = APIRouter()
 @beartype
 async def pong(*, settings: Settings = Depends(get_settings)) -> dict[str, str | bool]:
     return {
-        "ping": "pong5",
+        "ping": "pong",
         "environment": settings.environment,
         "testing": settings.testing,
     }
+
+
+@router.get("/version")
+@beartype
+async def version() -> dict[str, str]:
+    return {"version": __version__}
--- src/tests/test_ping.py
+++ src/tests/test_ping.py
@@ -1,3 +1,5 @@
+from re import search
+
 from beartype import beartype
 from fastapi.testclient import TestClient
 from starlette.status import HTTP_200_OK
@@ -7,4 +9,13 @@ from starlette.status import HTTP_200_OK
 def test_ping(*, test_app: TestClient) -> None:
     response = test_app.get("/ping")
     assert response.status_code == HTTP_200_OK
-    assert response.json() == {"environment": "dev", "ping": "pong5", "testing": "True"}
+    assert response.json() == {"environment": "dev", "ping": "pong", "testing": "True"}
+
+
+@beartype
+def test_version(*, test_app: TestClient) -> None:
+    response = test_app.get("/version")
+    assert response.status_code == HTTP_200_OK
+    assert set(json := response.json()) == {"version"}
+    version = json["version"]
+    assert search(r"^(\d+)\.(\d+)\.(\d+)$", version)
