6bd8c421d405d83bf6bd74ce718f31af6c9e2bcb
Derek Wan <d.wan@icloud.com>
2023-04-14 08:54:35 +0800

    Fix `docker-compose`, add GH actions (#7)

--- src/app/api/ping.py
+++ src/app/api/ping.py
@@ -10,7 +10,7 @@ router = APIRouter()
 @beartype
 async def pong(*, settings: Settings = Depends(get_settings)) -> dict[str, str | bool]:
     return {
-        "ping": "pong4",
+        "ping": "pong5",
         "environment": settings.environment,
         "testing": settings.testing,
     }
