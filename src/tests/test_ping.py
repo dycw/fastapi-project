f22fdf4893564256f5302f7d3479f5c214287ef3
Derek Wan <d.wan@icloud.com>
2023-04-15 14:53:31 +0900

    Fix `environment` in `typed-settings` (#16)

--- src/tests/test_ping.py
+++ src/tests/test_ping.py
@@ -1,18 +1,17 @@
-from re import search
-
 from app.config import Environment
 from beartype import beartype
 from fastapi.testclient import TestClient
+from semver.version import Version
 from starlette.status import HTTP_200_OK
 
 
 @beartype
-def test_env(*, test_app: TestClient) -> None:
-    response = test_app.get("/environment")
+def test_debug(*, test_app: TestClient) -> None:
+    response = test_app.get("/debug")
     assert response.status_code == HTTP_200_OK
-    assert set(json := response.json()) == {"environment"}
-    environment = Environment[json["environment"]]
-    assert environment in Environment
+    assert set(json := response.json()) == {"environment", "version"}
+    assert isinstance(Environment[json["environment"]], Environment)
+    assert isinstance(Version(*json["version"]), Version)
 
 
 @beartype
@@ -20,12 +19,3 @@ def test_ping(*, test_app: TestClient) -> None:
     response = test_app.get("/ping")
     assert response.status_code == HTTP_200_OK
     assert response.json() == {"environment": "dev", "ping": "pong", "testing": "True"}
-
-
-@beartype
-def test_version(*, test_app: TestClient) -> None:
-    response = test_app.get("/version")
-    assert response.status_code == HTTP_200_OK
-    assert set(json := response.json()) == {"version"}
-    version = json["version"]
-    assert search(r"^(\d+)\.(\d+)\.(\d+)$", version)
