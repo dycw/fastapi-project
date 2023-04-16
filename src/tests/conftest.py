3511481648f96a2e23de1bd2531a7b533501c35c
Derek Wan <d.wan@icloud.com>
2023-04-16 12:08:51 +0900

    Move into `typed-settings` properly

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,28 +1,15 @@
 from collections.abc import Iterator
-from os import environ
-from typing import cast
 
-from app.config import Settings, get_settings
+from app.config import SETTINGS
 from app.main import create_application
-from beartype import beartype
 from fastapi.testclient import TestClient
-from pydantic import AnyUrl
 from pytest import fixture
 from tortoise.contrib.fastapi import register_tortoise
-
-
-@beartype
-def get_settings_override() -> Settings:
-    return Settings(
-        testing=cast(bool, 1),
-        database_url=cast(AnyUrl, environ.get("DATABASE_TEST_URL")),
-    )
 
 
 @fixture(scope="module")
 def test_app() -> Iterator[TestClient]:
     app = create_application()
-    app.dependency_overrides[get_settings] = get_settings_override
     with TestClient(app) as test_client:
         yield test_client
 
@@ -30,10 +17,9 @@ def test_app() -> Iterator[TestClient]:
 @fixture(scope="module")
 def test_app_with_db() -> Iterator[TestClient]:
     app = create_application()
-    app.dependency_overrides[get_settings] = get_settings_override
     register_tortoise(
         app,
-        db_url=environ.get("DATABASE_TEST_URL"),
+        db_url=SETTINGS.database_test_url,
         modules={"models": ["app.models.tortoise"]},
         generate_schemas=True,
         add_exception_handlers=True,
