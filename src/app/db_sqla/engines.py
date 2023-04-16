6e8b3463ce913464ce5394f9a848e2243cc2132d
Derek Wan <d.wan@icloud.com>
2023-04-16 12:29:50 +0900

    Add basic `sqlalchemy`

---
+++ src/app/db_sqla/engines.py
@@ -0,0 +1,27 @@
+from collections.abc import Callable, Iterator
+
+from beartype import beartype
+from sqlalchemy import Engine, create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+from app.config import SETTINGS
+
+ENGINE = create_engine(SETTINGS.database_url)
+
+
+@beartype
+def create_yield_sess(engine: Engine, /) -> Callable[[], Iterator[Session]]:
+    """Create a `yield_sess` iterator for a given engine."""
+    sess_maker = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+    def yield_sess() -> Iterator[Session]:
+        sess = sess_maker()
+        try:
+            yield sess
+        finally:
+            sess.close()
+
+    return yield_sess
+
+
+yield_sess = create_yield_sess(ENGINE)
---
+++ src/app/db_sqla/schemas/users.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db_sqla.schemas.base import Base
+
+
+class UserDB(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(  # noqa: A003
+        init=False, primary_key=True, index=True
+    )
+    email: Mapped[str]
+    password: Mapped[str]
