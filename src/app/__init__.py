c7cb8fe81e23389092ca415c03925783de3b4e12
Derek Wan <d.wan@icloud.com>
2023-04-16 23:26:49 +0900

    Ignore `BeartypeDecorHintPep585DeprecationWarning` (#19)

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,1 +1,8 @@
-__version__ = "0.1.17"
+from warnings import simplefilter
+
+from beartype.roar import BeartypeDecorHintPep585DeprecationWarning
+
+simplefilter("ignore", category=BeartypeDecorHintPep585DeprecationWarning)
+
+
+__version__ = "0.1.18"
