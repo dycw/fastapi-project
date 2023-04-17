565d505406315b5c05b6ed612d9751b23f34ce53
Derek Wan <d.wan@icloud.com>
2023-04-17 15:32:11 +0900

    Add `pgadmin` (#22)

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,8 +1,10 @@
 from warnings import simplefilter
 
 from beartype.roar import BeartypeDecorHintPep585DeprecationWarning
+from semver.version import Version
 
 simplefilter("ignore", category=BeartypeDecorHintPep585DeprecationWarning)
 
 
-__version__ = "0.1.19"
+__version__ = "0.1.20"
+VERSION = Version.parse(__version__)
--- src/app/api/home.py
+++ src/app/api/home.py
@@ -1,8 +1,14 @@
+import datetime as dt
+
 from beartype import beartype
 from fastapi import APIRouter, HTTPException, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
+from utilities.zoneinfo import HONG_KONG
+
+from app import VERSION
+from app.config import ENVIRONMENT
 
 templates = Jinja2Templates(directory="templates")
 
@@ -14,7 +20,15 @@ router = APIRouter()
 @beartype
 async def home(*, request: Request) -> Response:
     try:
-        return templates.TemplateResponse("index.html", {"request": request})
+        return templates.TemplateResponse(
+            "index.html",
+            {
+                "request": request,
+                "datetime": dt.datetime.now(tz=HONG_KONG),
+                "environment": ENVIRONMENT,
+                "version": VERSION,
+            },
+        )
     except Exception as error:  # noqa: BLE001
         raise HTTPException(
             status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail=str(error)
